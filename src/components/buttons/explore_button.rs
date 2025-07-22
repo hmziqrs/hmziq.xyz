0fcde727fb05e838b7a4f13478fda249483046f9
hmziqrs <hmziqrs@gmail.com>
2025-07-22 19:19:29 +0500

    Update explore button hover animation style

--- src/components/buttons/explore_button.rs
+++ src/components/buttons/explore_button.rs
@@ -11,16 +11,16 @@ pub fn ExploreButton(props: ExploreButtonProps) -> Element {
     rsx! {
         a {
             href: "{props.href}",
-            class: "inline-block px-14 py-5 border border-white text-white hover:text-black transition-all duration-300 ease-in-out relative overflow-hidden tracking-wider uppercase text-sm hover:shadow-[0_0_30px_rgba(255,255,255,0.5)] group",
-            
+            class: "inline-block px-18 py-6 border border-white text-white hover:text-black transition-all duration-300 ease relative overflow-hidden tracking-wider uppercase text-sm hover:shadow-[0_0_30px_rgba(255,255,255,0.5)] group",
+
             // Expanding circle background
             div {
-                class: "absolute top-1/2 left-1/2 w-0 h-0 bg-white rounded-full transition-all duration-300 ease-in-out transform -translate-x-1/2 -translate-y-1/2 group-hover:w-[300px] group-hover:h-[300px]"
+                class: "absolute top-1/2 left-1/2 w-0 h-0 bg-white rounded-full hover:rounded-full transition-all duration-300 ease transform -translate-x-1/2 -translate-y-1/2 group-hover:w-[250px] group-hover:h-[250px]"
             }
-            
+
             // Text content
             span {
-                class: "relative z-10",
+                class: "relative z-10 tracking-[0.2rem]",
                 "{props.text}"
             }
         }
